7a6c1d47723187fcb84419d845c1dab5ce27d09b
andreseichi <andre_seichi@hotmail.com>
2022-05-19 13:31:52 -0300

    initial home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import './styles/global.scss';
+
+import styles from './styles/home.module.scss';
+
 export function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <h1>
+          <span>Song</span> Lyrics
+        </h1>
+        <p>
+          Get lyrics from a <span>song</span>
+        </p>
+      </section>
+
+      <form className={styles.formContainer}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="artist">Artist</label>
+          <input type="text" id="artist" />
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <label htmlFor="music">Music</label>
+          <input type="text" id="music" />
+        </div>
+      </form>
+    </main>
   );
 }
